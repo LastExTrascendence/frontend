4422829d74c3a521d6f3bf2bfdb3147efdca1912
chan <chanhihi55@gmail.com>
2023-12-14 23:03:25 +0900

    feat: navlink wrapper 추가 및 followlist 추가

--- frontend/src/app/(overview)/layout.tsx
+++ frontend/src/app/(overview)/layout.tsx
@@ -1,10 +1,14 @@
-import  SideNav from '@/ui/overview/sidenav';
+import NavLinksWrapper from "@/ui/overview/nav-links-wrapper";
+import SideNavFollowList from "@/ui/overview/sidenav-follow-list";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
+      <div className="w-[100px] h-full bg-neutral-800">
+        <NavLinksWrapper />
+      </div>
+      <div className="w-[300px] h-full bg-zinc-800">
+        <SideNavFollowList />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
