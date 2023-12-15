0c2b9f6c1016aeccded5270f064f07eaccb68bf6
chan <chanhihi55@gmail.com>
2023-12-15 18:10:25 +0900

    style: Update layout component imports and styles

--- frontend/src/app/(overview)/layout.tsx
+++ frontend/src/app/(overview)/layout.tsx
@@ -1,13 +1,13 @@
-import NavLinksWrapper from "@/ui/overview/nav-links-wrapper";
-import SideNavFollowList from "@/ui/overview/sidenav-follow-list";
+import NavLinksWrapper from "@/ui/overview/sidenavbar/nav-links-wrapper";
+import SideNavFollowList from "@/ui/overview/sidenavbar/sidenav-follow-list";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-[100px] h-full bg-neutral-800">
+      <div className="h-full w-[100px] bg-neutral-800">
         <NavLinksWrapper />
       </div>
-      <div className="w-[300px] h-full bg-zinc-800">
+      <div className="h-full w-[300px] bg-zinc-800">
         <SideNavFollowList />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
