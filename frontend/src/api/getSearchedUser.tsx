146e89fc8dde700a314b135d46fac51063fee34e
chan <chanhihi55@gmail.com>
2023-12-17 00:44:08 +0900

    feat: Add getSearchedUser API function

---
+++ frontend/src/api/getSearchedUser.tsx
@@ -0,0 +1,17 @@
+import { UserlistProps } from "@/lib/definitions";
+import customFetch from "@/utils/fetch";
+
+export async function getSearchedUser(
+  searchedUser: string,
+): Promise<UserlistProps[]> {
+  try {
+    const response = await customFetch(`/user/search?query=${searchedUser}`);
+    const data = await response.json();
+    return data as UserlistProps[];
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export default getSearchedUser;
