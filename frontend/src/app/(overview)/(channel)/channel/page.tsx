ee2e7d7160f4dac9d07a29318cc823c25e397bec
chan <chanhihi55@gmail.com>
2023-12-21 16:21:36 +0900

    refactor: Refactor channel page components

--- frontend/src/app/(overview)/(channel)/channel/page.tsx
+++ frontend/src/app/(overview)/(channel)/channel/page.tsx
@@ -1,73 +1,45 @@
 "use client";
 
 import React, { useState } from "react";
-import Search from "@/ui/overview/channel/search-channel";
+import SearchChannel from "@/ui/overview/channel/search-channel";
 import chats from "@/lib/chat-data";
 import Modal from "@/components/Modal";
 import NewChannel from "@/ui/overview/channel/new-channel";
-import Link from "next/link";
+import NewChannelButton from "@/ui/overview/channel/new-channel-button";
+import ChannelList from "@/ui/overview/channel/channel-list";
+import JoinChannel from "@/ui/overview/channel/join-channel";
 
 export default function Page() {
   const [showModal, setShowModal] = useState(false);
   const [selectedChat, setSelectedChat] = useState(null);
 
-  const openModal = (chat) => {
+  const toggleJoinChannelModal = (chat) => {
     setSelectedChat(chat);
   };
 
-  const toggleModal = () => {
+  const toggleNewChannelModal = () => {
     setShowModal(!showModal);
   };
 
   return (
-    <div className="flex h-full w-full scroll-m-0 flex-col">
-      <div className="flex h-[50px] flex-row">
-        <Search placeholder="Search channels" />
-        <button
-          type="button"
-          className="h-full w-48 rounded-[30px] bg-buttonColor"
-          onClick={toggleModal}
-        >
-          NEW
-        </button>
-      </div>
-      <div className="h-full w-full rounded-[20px] bg-zinc-800">
-        <div className="flex flex-row border-b-2">
-          <p className="mr-4 flex-1">Channel</p>
-          <p className="mr-4 flex-1">Creator</p>
-          <p className="mr-4 flex-1">Users</p>
-          <p className="mr-4 flex-1">Type</p>
+    <div className="bg-gray-60 relative h-full w-full items-center justify-center p-3">
+      <div className="flex h-full w-full scroll-m-0 flex-col">
+        <div className="flex h-4/5 min-h-[60px] flex-row">
+          <SearchChannel placeholder="Search channels" />
+          <NewChannelButton toggleModal={toggleNewChannelModal} />
         </div>
-        <div>
-          {chats.map((chat) => (
-            <div
-              key={chat.id}
-              className="flex cursor-pointer flex-row border-b-2"
-              onClick={() => openModal(chat)}
-            >
-              <p className="mr-4 flex-1">{chat.channel}</p>
-              <p className="mr-4 flex-1">{chat.creator}</p>
-              <p className="mr-4 flex-1">{chat.users}</p>
-              <p className="mr-4 flex-1">{chat.type}</p>
-            </div>
-          ))}
-        </div>
-        {selectedChat && (
-          <Modal chat={selectedChat} onClose={() => setSelectedChat(null)}>
-            <div className="text-black">
-              <p>Channel: {selectedChat.channel}</p>
-              <p>Creator: {selectedChat.creator}</p>
-              <p>Users: {selectedChat.users}</p>
-              <p>Type: {selectedChat.type}</p>
-              <Link href={`/channel/${selectedChat.id}`}>Go to Channel</Link>
-            </div>
-          </Modal>
-        )}
+        <ChannelList chats={chats} openModal={toggleJoinChannelModal} />
       </div>
 
+      {selectedChat && (
+        <Modal onClose={() => setSelectedChat(null)}>
+          <JoinChannel selectedChat={selectedChat} />
+        </Modal>
+      )}
+
       {showModal && (
-        <Modal onClose={toggleModal}>
-          <NewChannel onClose={toggleModal} />
+        <Modal onClose={toggleNewChannelModal}>
+          <NewChannel onClose={toggleNewChannelModal} />
         </Modal>
       )}
     </div>
