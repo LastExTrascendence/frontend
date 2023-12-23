96788af785bfe7cfbad060bf7046d943ce27498c
jusohn <jusohn@student.42seoul.kr>
2023-12-23 22:06:18 +0900

    fix: main page 업데이트 #25

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,92 @@
-import MainLoginButton from "@/ui/mainpage/main-login-button";
-import LetLogo from "@/ui/mainpage/let-logo";
-import Mainpage from "@/ui/mainpage/mainpage";
+"use client";
+import styled from "styled-components";
+import Image from "next/image";
 import LinksList from "@/ui/mainpage/links";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <MainLoginButton />
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"></div>
-        <LetLogo />
-      </div>
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Mainpage />
-      </div>
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+    <MainPageStyled>
+      <TopNavStyled>
+        <Image
+          src="/info.svg"
+          alt="Logo"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+        <TopNavButtonContainer>
+          <Image
+            src="/logout.svg"
+            alt="Logo"
+            width={40}
+            height={40}
+            layout="fixed"
+          />
+          Logout
+        </TopNavButtonContainer>
+      </TopNavStyled>
+      <MainLogoContainer>
+        <MainLogoTitleStyled>L.E.T</MainLogoTitleStyled>
+        <Image
+          src="/LET_logo.svg"
+          alt="Logo"
+          width={120}
+          height={120}
+          layout="absolute"
+        />
+      </MainLogoContainer>
+      <ButtonGroupContainer>
         <LinksList />
-      </div>
-    </main>
+      </ButtonGroupContainer>
+    </MainPageStyled>
   );
 }
+
+const MainPageStyled = styled.main`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 6rem;
+`;
+
+const TopNavStyled = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  padding: 0 2rem;
+`;
+
+const TopNavButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  color: var(--light-gray);
+`;
+
+const MainLogoContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const MainLogoTitleStyled = styled.div`
+  color: var(--white);
+  font-style: italic;
+  font-weight: 800;
+  font-size: 6rem;
+  margin-right: 2rem;
+`;
+
+const ButtonGroupContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
