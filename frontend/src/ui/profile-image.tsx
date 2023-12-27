78343c94d8001ca470e2de53627a16b6bcd8c3ae
jusohn <jusohn@student.42seoul.kr>
2023-12-27 14:17:44 +0900

    fix: 내 프로필 페이지 레이아웃 완성

--- frontend/src/ui/profile-image.tsx
+++ frontend/src/ui/profile-image.tsx
@@ -4,17 +4,23 @@ import Image from "next/image";
 export default function ProfileImage({
   width = 100,
   height = 100,
+  borderRadius = 20,
   showOutline = false,
+  showBorder = false,
 }: {
   width: number;
   height: number;
+  borderRadius?: number;
   showOutline?: boolean;
+  showBorder?: boolean;
 }) {
   return (
     <ProfileImageStyled
       $width={width}
       $height={height}
+      $borderRadius={borderRadius}
       $showOutline={showOutline}
+      $showBorder={showBorder}
     >
       <Image
         src="/default_profile.svg"
@@ -30,15 +36,19 @@ export default function ProfileImage({
 const ProfileImageStyled = styled.div<{
   $width?: number;
   $height?: number;
+  $borderRadius?: number;
   $showOutline?: boolean;
+  $showBorder?: boolean;
 }>`
   width: ${({ $width }) => $width}px;
   height: ${({ $height }) => $height}px;
-  border-radius: 20px;
+  border-radius: ${({ $borderRadius }) => $borderRadius}px;
   background: var(--light-gray);
   display: flex;
   justify-content: center;
   align-items: center;
   outline: ${({ $showOutline }) =>
     $showOutline ? "1px solid var(--black)" : ""};
+  border: ${({ $showBorder }) =>
+    $showBorder ? "5px solid var(--main-purple)" : ""};
 `;
