e43acbcbf5ed85a24cb8679ceaa2050cf60f2e3d
chan <chanhihi55@gmail.com>
2024-01-05 01:34:35 +0900

    fix: 부모뷰 수정

--- frontend/src/app/(overview)/layout.tsx
+++ frontend/src/app/(overview)/layout.tsx
@@ -21,7 +21,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
 const MainSectionStyled = styled.main`
   display: flex;
-  height: calc(100% - 90px);
+  height: calc(100vh - 90px);
   width: 100%;
   overflow: hidden;
 `;
