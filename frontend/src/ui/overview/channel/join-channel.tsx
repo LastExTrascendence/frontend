ed4e504114f866febca535057e4426c51989ffa0
chan <chanhihi55@gmail.com>
2024-01-05 01:34:11 +0900

    feat: add query string

--- frontend/src/ui/overview/channel/join-channel.tsx
+++ frontend/src/ui/overview/channel/join-channel.tsx
@@ -7,7 +7,9 @@ export default function ChannelList({ selectedChat }: { selectedChat: any }) {
       <p>Creator: {selectedChat.creator}</p>
       <p>Users: {selectedChat.users}</p>
       <p>Type: {selectedChat.type}</p>
-      <Link href={`/channel/${selectedChat.id}`}>Go to Channel</Link>
+      <Link href={`/channel/${selectedChat.id}?name=${selectedChat.channel}`}>
+        Go to Channel
+      </Link>
     </div>
   );
 }
