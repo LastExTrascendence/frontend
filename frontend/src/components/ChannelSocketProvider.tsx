ab0b39ccf032c585797e3570425f5d9636fdc1da
chan <chanhihi55@gmail.com>
2024-01-11 21:42:51 +0900

    feat: leave channel socket logic

--- frontend/src/components/ChannelSocketProvider.tsx
+++ frontend/src/components/ChannelSocketProvider.tsx
@@ -3,15 +3,14 @@
 import { createContext, useContext, useEffect, useState, useMemo } from "react";
 import io from "socket.io-client";
 import { getCookie } from "@/api/cookie/cookies";
-
-type ChannelSocketContextType = {
-  channelSocket: any | null;
-  isChannelConnected: boolean;
-};
+import { ChannelSocketContextType } from "@/types/type/channel-socket.type";
+import { useRouter } from "next/navigation";
 
 const ChannelSocketContext = createContext<ChannelSocketContextType>({
   channelSocket: null,
   isChannelConnected: false,
+  setChannelId: () => {},
+  setUserId: () => {},
 });
 
 export const useChannelSocket = () => useContext(ChannelSocketContext);
@@ -21,8 +20,11 @@ export default function ChannelSocketProvider({
 }: {
   children: React.ReactNode;
 }) {
+  const [channelId, setChannelId] = useState<number>(0);
+  const [userId, setUserId] = useState<number>(0);
   const [channelSocket, setChannelSocket] = useState<any | null>(null);
   const [isChannelConnected, setIsConnected] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     if (!channelSocket) {
@@ -31,8 +33,9 @@ export default function ChannelSocketProvider({
 
     channelSocket.on("disconnect", async () => {
       setIsConnected(false);
+      await router.replace("/channel");
     });
-  }, []);
+  }, [channelSocket]);
 
   useEffect(() => {
     const token = getCookie("access_token") ?? null;
@@ -51,13 +54,25 @@ export default function ChannelSocketProvider({
 
     setChannelSocket(socketInstance);
     return () => {
-      console.log("disconnect");
+      socketInstance.emit("leaveChannel", {
+        channelId,
+        userId,
+      });
+      console.log("leave channel", channelId, userId);
+
+      socketInstance.off("connect");
+      socketInstance.off("disconnect");
       socketInstance.disconnect();
     };
-  }, []);
+  }, [channelId, userId]);
 
   const contextValue = useMemo(
-    () => ({ channelSocket, isChannelConnected }),
+    () => ({
+      channelSocket,
+      isChannelConnected,
+      setChannelId,
+      setUserId,
+    }),
     [channelSocket, isChannelConnected],
   );
 
