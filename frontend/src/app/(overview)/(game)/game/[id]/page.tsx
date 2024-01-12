0d97318787760b58a9881703fe9348eed4d95be0
chan <chanhihi55@gmail.com>
2024-01-12 21:11:03 +0900

    feat: game channel

--- frontend/src/app/(overview)/(game)/game/[id]/page.tsx
+++ frontend/src/app/(overview)/(game)/game/[id]/page.tsx
@@ -1,145 +1,44 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
-import { useGameSocket } from "@/components/GameSocketProvider";
+import { useSearchParams } from "next/navigation";
+import Image from "next/image";
+
+import { useState, useEffect, useRef } from "react";
 import { useRecoilValue } from "recoil";
 import { myState } from "@/recoil/atom";
-import Image from "next/image";
-import { useSearchParams } from "next/navigation";
 
-// channel 입장시 받아야할 정보
-// 1. 채널 주인 / 관리자
-// 2. 채널 멤버
+import { Message, ChatAttendees } from "@/types/interface/chat.interface";
+import { useChannelSocket } from "@/components/ChannelSocketProvider";
+import useChannelListener from "@/hooks/useChannelListener";
+import useUserListListener from "@/hooks/useUserListListener";
+import useUserListComposer from "@/hooks/useUserListComposer";
+import useChannelHandler from "@/hooks/useChannelHandler";
+import useChannelEnter from "@/hooks/useChannelEnter";
 
-// TODO
-// 컴포넌트 분리
-
-interface Message {
-  time: string | Date;
-  sender: string;
-  content: string;
-}
-
-interface User {
-  id: number;
-  nickname: string;
-  avatar: string;
-  role: string;
-  mute: boolean;
-}
-
-const mockUser = [
-  {
-    id: 1,
-    nickname: "misukim",
-    avatar: "https://via.placeholder.com/36x36",
-  },
-  {
-    id: 2,
-    nickname: "chanheki",
-    avatar: "https://via.placeholder.com/36x36",
-  },
-];
-
-const getRoleIcon = (role: string) => {
-  switch (role) {
-    case "CREATOR":
-      return <Image src="/creator.svg" alt="Creator" width={18} height={18} />;
-    case "OPERATOR":
-      return (
-        <Image src="/operator.svg" alt="Operator" width={18} height={18} />
-      );
-    default:
-      return <div style={{ width: "18px", height: "18px" }} />;
-  }
-};
-
-const getAdminIcon = (role: string) => {
-  switch (role) {
-    case "CREATOR":
-      return (
-        <div className="ml-auto flex bg-red-800">
-          <button
-            type="button"
-            className="p-1"
-            onClick={() => handleApiRequest("user-mute")}
-          >
-            <Image
-              src="/user-mute.svg"
-              alt="User Mute"
-              width={18}
-              height={18}
-            />
-          </button>
-          <button
-            type="button"
-            className="p-1"
-            onClick={() => handleApiRequest("user-kick")}
-          >
-            <Image
-              src="/user-kick.svg"
-              alt="User Kick"
-              width={18}
-              height={18}
-            />
-          </button>
-          <button type="button" onClick={() => handleApiRequest("user-block")}>
-            <Image
-              src="/user-block.svg"
-              alt="User Block"
-              width={18}
-              height={18}
-            />
-          </button>
-        </div>
-      );
-
-    case "OPERATOR":
-      return (
-        <div className="ml-auto">
-          <Image src="/operator.svg" alt="Operator" width={18} height={18} />
-        </div>
-      );
-
-    case "USER":
-    default:
-      return null;
-  }
-};
+import getAdminIcon from "@/ui/overview/channel/get-admin-icon";
+import GetRoleIcon from "@/ui/overview/channel/get-role-icon";
+import MessageItem from "@/ui/overview/channel/message-item";
+import MessageInput from "@/ui/overview/channel/message-input";
+import GrowBlank from "@/ui/grow-blank";
+import changeRole from "@/api/socket/chat/changeRole";
 
 export default function Page({ params }: { params: { id: string } }) {
   const myInfo = useRecoilValue(myState);
   const [messages, setMessages] = useState<Message[]>([]);
+  const { channelSocket, isChannelConnected, setChannelId, setUserId } =
+    useChannelSocket();
+
   const [currentMessage, setCurrentMessage] = useState("");
-  const { channelSocket, isConnected } = useGameSocket();
-  const [userList, setUserList] = useState<User[]>();
+  const [userList, setUserList] = useState<ChatAttendees[]>();
   const [myRole, setMyRole] = useState<string>("USER");
   const messagesEndRef = useRef(null);
   const searchParams = useSearchParams();
   const name = searchParams.get("name");
+  const messageRef = useRef("");
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
-
-  useEffect(() => {
-    console.log("user", userList);
-
-    if (!userList) {
-      setMyRole("USER");
-      return;
-    }
-
-    const matchingUser = userList.find(
-      (user) => user.nickname === myInfo.nickname,
-    );
-
-    if (matchingUser) {
-      setMyRole(matchingUser.role);
-    } else {
-      setMyRole("USER");
-    }
-  }, [userList]);
 
   useEffect(() => {
     scrollToBottom();
@@ -151,34 +50,20 @@ export default function Page({ params }: { params: { id: string } }) {
     }
   };
 
-  useEffect(() => {
-    if (!channelSocket) {
-      return;
-    }
+  useChannelHandler(myInfo.id, params.id, setUserId, setChannelId);
+  useChannelEnter(channelSocket, isChannelConnected, myInfo.id, name);
+  useChannelListener(channelSocket, setMessages);
+  useUserListListener(channelSocket, setUserList);
+  useUserListComposer(userList, myInfo.nickname, setMyRole);
 
-    const messageListener = (message: Message) => {
-      setMessages((prevMessages) => [...prevMessages, message]);
-    };
+  // useChannelListener(channelSocket, setMessages, setUserList, myInfo, name);
 
-    channelSocket.on("msgToClient", messageListener);
-
-    channelSocket.emit("enter", myInfo);
-
-    channelSocket.on("userList", (userListData) => {
-      setUserList(userListData);
-    });
-
-    // 클린업 함수
-    return () => {
-      channelSocket.off("msgToClient", messageListener);
-    };
-  }, [channelSocket]);
-
-  const sendMessage = (e) => {
+  const sendMessage = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
     if (currentMessage.trim()) {
       channelSocket.emit("msgToServer", {
         time: new Date(),
+        title: name,
         sender: myInfo.id,
         content: currentMessage,
       });
@@ -189,76 +74,59 @@ export default function Page({ params }: { params: { id: string } }) {
   return (
     <div className="m-12 flex max-h-[1833px] min-h-[400px] w-full min-w-[400px] flex-row content-center items-start">
       <div className="flex h-full w-full flex-col bg-chatColor p-9">
-        <div class="relative h-[200px] w-full max-w-[950px]">
-          <div class="absolute left-0 top-0 h-full w-full rounded-[20px] bg-gray-800"></div>
+        <div className="flex h-[200px] w-full max-w-[950px]">
+          {/* 왼쪽 표 형태 섹션 */}
+          <div className="flex flex-grow flex-col rounded-l-[20px] bg-gray-800">
+            {/* 타이틀 부분 */}
+            <div className="flex flex-grow">
+              <div className="grid flex-grow place-items-center text-[28px] font-normal text-white">
+                Mode
+              </div>
+              <div className="grid flex-grow place-items-center text-[28px] font-normal text-white">
+                Type
+              </div>
+              <div className="grid flex-grow place-items-center text-[28px] font-normal text-white">
+                Speed
+              </div>
+            </div>
+            {/* 하단 텍스트들 */}
+            <div className="flex flex-grow">
+              <div className="grid flex-grow place-items-center text-[28px] font-normal text-white">
+                LADDER
+              </div>
+              <div className="grid flex-grow place-items-center text-[28px] font-normal text-white">
+                Original
+              </div>
+              <div className="grid flex-grow place-items-center text-[28px] font-normal text-white">
+                150%
+              </div>
+            </div>
+          </div>
 
-          {/* <!-- 타이틀들 --> */}
-          <div class="font-['Noto Sans KR'] absolute left-1/4 top-[37px] text-[28px] font-normal text-white md:left-[267px]">
-            Type
-          </div>
-          <div class="font-['Noto Sans KR'] absolute left-1/2 top-[37px] text-[28px] font-normal text-white md:left-[462px]">
-            Speed
-          </div>
-          <div class="font-['Noto Sans KR'] absolute left-[10%] top-[37px] text-[28px] font-normal text-white md:left-[65px]">
-            Mode
-          </div>
-
-          {/* <!-- 하단 텍스트들 --> */}
-          <div class="font-['Noto Sans KR'] absolute left-[10%] top-[98px] text-[28px] font-normal text-white md:left-[48px]">
-            LADDER
-          </div>
-          <div class="font-['Noto Sans KR'] absolute left-1/4 top-[99px] text-[28px] font-normal text-white md:left-[247px]">
-            Original
-          </div>
-          <div class="font-['Noto Sans KR'] absolute left-1/2 top-[100px] text-[28px] font-normal text-white md:left-[467px]">
-            150%
-          </div>
-
-          <div class="absolute left-[75%] top-[18px] inline-flex h-40 w-60 items-center justify-center md:left-[663px]">
+          {/* 오른쪽 이미지 섹션 */}
+          <div className="flex h-full w-60 items-center justify-center rounded-r-[20px] bg-gray-800">
             <Image src="/map.svg" alt="basic map" width={240} height={160} />
           </div>
         </div>
 
-        <div className="content-start items-center overflow-y-auto">
+        <div className="content-start items-center overflow-y-scroll">
           {messages.map((message, index) => (
-            <div
-              key={index}
-              className="grid grid-cols-[auto_auto_1fr] gap-4 rounded-lg px-2 pb-1 text-base text-white hover:bg-gray-700"
-            >
-              <span className="max-w-[100px] overflow-hidden">
-                {message.time}
-              </span>
-              <span className="max-w-[100px] overflow-hidden">
-                {message.sender}
-              </span>
-              <span>{message.content}</span>
-            </div>
+            <MessageItem key={index} message={message} />
           ))}
           <div ref={messagesEndRef} />
         </div>
-
-        <div className="my-2 w-full grow" />
-
-        <div className="bg-chatInputColor flex min-h-[35px] w-full rounded-md shadow-sm">
-          <input
-            className="placeholder:text-muted-foreground focus-visible:ring-ring border-input focus-visible:false flex-1 rounded-md rounded-l-md bg-transparent p-2 text-sm text-white shadow-sm transition-colors focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
-            type="text"
-            placeholder={`Send message to ${name}`}
-            value={currentMessage}
-            onChange={(e) => setCurrentMessage(e.target.value)}
-            onKeyDown={handleKeyDown}
-          />
-          <button
-            type="submit"
-            className="bg-primary hover:bg-primary/90 rounded-r-md text-sm text-white transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50"
-            onClick={(e) => sendMessage(e)}
-          >
-            <Image src="/send.svg" alt="SendButton" width={30} height={30} />
-          </button>
-        </div>
+        <GrowBlank />
+        <MessageInput
+          currentMessage={currentMessage}
+          setCurrentMessage={setCurrentMessage}
+          // messageRef={messageRef}
+          handleKeyDown={handleKeyDown}
+          sendMessage={sendMessage}
+          name={name}
+        />
       </div>
 
-      <div className="flex h-full flex-col bg-userInfoColor p-9">
+      <div className="hidden h-full min-w-[300px] max-w-[350px] flex-col overflow-y-scroll bg-userInfoColor p-9 md:block">
         <div className="font-['Noto Sans KR'] text-4xl font-normal text-white">
           #{name}
         </div>
@@ -269,25 +137,36 @@ export default function Page({ params }: { params: { id: string } }) {
                 <Image
                   className={
                     user.role === "CREATOR"
-                      ? "border-indigoColor h-[35.57px] w-[35.57px] rounded-[32px] border-4"
+                      ? "h-[35.57px] w-[35.57px] rounded-[32px] border-4 border-indigoColor"
                       : "rounded-full border border-black"
                   }
                   width={36}
                   height={36}
                   // src={user.avatar}
                   src="/default_profile.svg"
-                  alt={user.nickname}
+                  alt={user.nickname || ""}
                 />
 
-                {getRoleIcon(user.role)}
+                <GetRoleIcon
+                  myRole={myRole}
+                  userRole={user.role}
+                  changeRole={() =>
+                    changeRole(channelSocket, name, myInfo.id, user.nickname)
+                  }
+                />
 
                 <span className="font-['Noto Sans KR'] text-base font-normal text-white">
                   {user.nickname}
                 </span>
 
-                {myInfo.nickname !== user.nickname
-                  ? getAdminIcon(myRole)
-                  : null}
+                {myInfo.nickname !== user.nickname &&
+                  getAdminIcon({
+                    role: myRole,
+                    socket: channelSocket,
+                    title: name,
+                    myId: myInfo.id,
+                    user,
+                  })}
               </div>
             ))}
         </div>
