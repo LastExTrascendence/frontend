8596871d26ac9a97722c6b63a0d840aec139e853
chan <chanhihi55@gmail.com>
2024-01-16 21:29:30 +0900

    feat: game match socket

--- frontend/src/components/SocketProvider.tsx
+++ frontend/src/components/SocketProvider.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { createContext, useContext, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import io from "socket.io-client";
 import { getCookie } from "@/api/cookie/cookies";
+import { useRecoilValue } from "recoil";
+import { myState } from "@/recoil/atom";
 
 interface ConnectInfo {
   sender: number; // mystate id
@@ -12,11 +15,15 @@ interface ConnectInfo {
 type SocketContextType = {
   socket: any | null;
   isConnected: boolean;
+  enterQueue: () => void;
+  exitQueue: () => void;
 };
 
 const SocketContext = createContext<SocketContextType>({
   socket: null,
   isConnected: false,
+  enterQueue: () => { },
+  exitQueue: () => { }
 });
 
 export const useSocket = () => useContext(SocketContext);
@@ -28,6 +35,8 @@ export default function SocketProvider({
 }) {
   const [socket, setSocket] = useState<any | null>(null);
   const [isConnected, setIsConnected] = useState(false);
+  const myInfo = useRecoilValue(myState);
+  const router = useRouter();
 
   useEffect(() => {
     if (!socket) {
@@ -60,8 +69,37 @@ export default function SocketProvider({
     };
   }, []);
 
+  const enterQueue = () => {
+    if (socket) {
+      console.log("enterQueue", myInfo);
+      socket.emit("enterQueue", { userId: myInfo.id });
+    }
+  };
+
+  const exitQueue = () => {
+    if (socket) {
+      console.log("exitQueue", myInfo);
+      socket.emit("exitQueue", { userId: myInfo.id });
+    }
+  };
+
+  // queue connect handler
+  useEffect(() => {
+    if (!socket) {
+      return;
+    }
+
+    socket.on("gameMatch", async ({ gameId, title }: { gameId: string, title: string }) => {
+      await router.replace(`/game/${gameId}?name=${title}`);
+    });
+
+    return () => {
+      socket.off("gameMatch");
+    };
+  }, [socket]);
+
   return (
-    <SocketContext.Provider value={{ socket, isConnected }}>
+    <SocketContext.Provider value={{ socket, isConnected, enterQueue, exitQueue }}>
       {children}
     </SocketContext.Provider>
   );
