0250187f6b2a1ee2e72b731d7e387c2ccb3440de
jusohn <jusohn@student.42seoul.kr>
2024-01-19 02:06:33 +0900

    fix: NewGameModal 에 focus:bgColor 제거

--- frontend/src/components/Modals/NewGameChannelModal/NewGameChannelModal.tsx
+++ frontend/src/components/Modals/NewGameChannelModal/NewGameChannelModal.tsx
@@ -81,7 +81,7 @@ export default function NewGameChannelModal({
               <input
                 type="text"
                 name="game"
-                className="mt-2 rounded bg-buttonColor p-2 text-white focus:bg-violet-400 focus:outline-none focus:ring-2"
+                className="mt-2 rounded bg-buttonColor p-2 text-white focus:outline-none focus:ring-2"
                 maxLength={20}
                 minLength={1}
                 onChange={(e) => setTitle(e.target.value)}
@@ -97,17 +97,18 @@ export default function NewGameChannelModal({
               />
             </label>
             <div
-              className={`relative overflow-hidden transition-all duration-500 ease-in-out ${channelPolicy === ChannelPolicy.PRIVATE
-                ? "mb-3 max-h-40"
-                : "max-h-0"
-                }`}
+              className={`relative overflow-hidden transition-all duration-500 ease-in-out ${
+                channelPolicy === ChannelPolicy.PRIVATE
+                  ? "mb-3 max-h-40"
+                  : "max-h-0"
+              }`}
             >
               <label className="flex flex-col items-start justify-center">
                 Password
                 <input
                   type="password"
                   name="password"
-                  className="mt-2 rounded bg-buttonColor p-2 text-white focus:bg-violet-400 focus:outline-none focus:ring-2"
+                  className="mt-2 rounded bg-buttonColor p-2 text-white focus:outline-none focus:ring-2"
                   maxLength={10}
                   minLength={1}
                   onChange={(e) => setPassword(e.target.value)}
