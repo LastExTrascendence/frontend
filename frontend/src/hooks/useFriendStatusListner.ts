4a4091ea36df1834dda233f316e7a2e42c13e114
chan <chanhihi55@gmail.com>
2024-01-25 03:33:59 +0900

    fix: add optional and exception condition

--- frontend/src/hooks/useFriendStatusListner.ts
+++ frontend/src/hooks/useFriendStatusListner.ts
@@ -3,17 +3,18 @@
 import { useEffect } from "react";
 import { UserFriendListResponseDto } from "@/types/dto/user.dto";
 import { UserInfoDto } from "@/types/interface/user.interface";
+import { STATUS_400_BAD_REQUEST } from "@/types/constants/status-code";
 
 export default function useFriendStatusListner({ socket, friendsList, setFriendsList }: { socket: any, friendsList: UserFriendListResponseDto, setFriendsList: any }) {
 
   useEffect(() => {
     if (!socket) return;
-    if (!friendsList) return;
+    if (friendsList === undefined && friendsList === STATUS_400_BAD_REQUEST) return;
 
     socket.on("followingStatus", (data: UserInfoDto) => {
-      if (!data) return;
+      if (!data || friendsList === STATUS_400_BAD_REQUEST) return;
 
-      const newFriendsList = friendsList.map((friend: { id: number; }) => {
+      const newFriendsList = friendsList?.map((friend: { id: number; }) => {
         if (friend.id === data.id) {
           return data;
         }
