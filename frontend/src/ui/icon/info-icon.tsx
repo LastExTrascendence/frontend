429ecc384557f2103f46df501f6f04cf3ead38a0
chan <chanhihi55@gmail.com>
2024-01-28 04:17:48 +0900

    test: change language button

--- frontend/src/ui/icon/info-icon.tsx
+++ frontend/src/ui/icon/info-icon.tsx
@@ -1,9 +1,32 @@
+import i18n from "i18next";
 import IconContainer from "@/ui/icon/icon-container";
+import { useTranslation } from "react-i18next";
 
 export default function InfoIcon() {
+  const { t } = useTranslation();
+
   const onClickInfo = (): void => {
-    // TODO: show info modal
+    const currentLang = i18n.language;
+    let newLang;
+
+    switch (currentLang) {
+      case "ko":
+        newLang = "en";
+        break;
+      case "en":
+        newLang = "fr";
+        break;
+      case "fr":
+        newLang = "ko";
+        break;
+      default:
+        newLang = "en";
+    }
+
+    i18n.changeLanguage(newLang);
   };
+
+
   return (
     <IconContainer
       width={40}
@@ -11,7 +34,7 @@ export default function InfoIcon() {
       iconPath="/info.svg"
       iconColor="var(--light-gray)"
       fontColor="var(--light-gray)"
-      text="Info"
+      text={t("info")}
       onClick={onClickInfo}
     />
   );
