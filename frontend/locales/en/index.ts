e17aa2ae260664198012c437721c14cc9d2ea836
chan <chanhihi55@gmail.com>
2024-01-28 04:12:43 +0900

    build: apply i18

---
+++ frontend/locales/en/index.ts
@@ -0,0 +1,11 @@
+import common_en from './common_en.json'
+import game_en from './game_en.json'
+import channel_en from './channel_en.json'
+import profile_en from './profile_en.json'
+
+export {
+  common_en,
+  game_en,
+  channel_en,
+  profile_en
+}
