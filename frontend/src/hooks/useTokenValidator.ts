d7df3433eadc17663fc991d0ddc55c2377a43b28
chan <chanhihi55@gmail.com>
2024-01-27 22:21:42 +0900

    feat: token exp validate and update recoil userdata

--- frontend/src/hooks/useTokenValidator.ts
+++ frontend/src/hooks/useTokenValidator.ts
@@ -1,16 +1,44 @@
+"use client";
+
+import { useEffect } from "react";
 import { usePathname, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+import { useRecoilState } from "recoil";
+import { myState } from "@/recoil/atom";
 import { getCookie } from "@/api/cookie/cookies";
+import { IToken } from "@/app/register/page";
 
 export default function useTokenValidator({ setIsClient }: { setIsClient: any }) {
   const pathname = usePathname();
   const router = useRouter();
+  const [myInfo, setMyInfo] = useRecoilState(myState);
 
   useEffect(() => {
     const token = getCookie("access_token");
     setIsClient(true);
 
-    if (!token && pathname !== "/login" && pathname !== "/login/otp" && pathname !== "/register") {
+    if (token) {
+      try {
+        const decodedToken: IToken = jwtDecode(token);
+        const isExpired = decodedToken.exp * 1000 < Date.now();
+        if (isExpired) {
+          throw new Error("Token expired");
+        }
+
+        if (myInfo.id === 0) {
+          setMyInfo(prevInfo => ({
+            ...prevInfo,
+            id: decodedToken.id,
+            nickname: decodedToken.nickname
+          }));
+        }
+
+      } catch (error) {
+        // 토큰 검증 실패
+        console.error("Token validation failed:", error);
+        router.push("/login");
+      }
+    } else if (pathname !== "/login" && pathname !== "/login/otp" && pathname !== "/register") {
       router.push("/login");
     }
   }, [pathname, router]);
